aaa57b15274bbc3ee3eec99bd67c8954a903d690
david361215 <1138890119@qq.com>
2023-02-08 10:43:33 +0800

    修改默认路由，移动端调试

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -6,10 +6,12 @@ import { Welcome } from '../components/welcome/Welcome';
 import { RouteRecordRaw } from 'vue-router';
 
 export const routes: RouteRecordRaw[] = [
-  { 
+  { path: '/', redirect: '/welcome' },
+  {
     path: '/welcome',
     component: Welcome,
     children: [
+      { path: '', redirect: '/welcome/1', },
       { path: '1', component: First, },
       { path: '2', component: Second, },
       { path: '3', component: Third, },
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,39 @@
 import { createApp } from 'vue'
 import { App } from './App'
-import { createRouter} from 'vue-router'
+import { createRouter } from 'vue-router'
 import { routes } from './config/routes';
 import { history } from './shared/history';
 
-const router = createRouter ({ history, routes })
+const router = createRouter({ history, routes })
 
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+
+// 如下代码是禁止用户在 ios 上双手缩放页面
+document.documentElement.addEventListener(
+  "touchstart",
+  function (event) {
+    if (event.touches.length > 1) {
+      event.preventDefault();
+    }
+  },
+  false
+);
+
+var lastTouchEnd = 0;
+document.documentElement.addEventListener(
+  "touchend",
+  function (event) {
+    var now = Date.now();
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+    lastTouchEnd = now;
+  },
+  false
+);
+
+document.addEventListener("gesturestart", function (event) {
+  event.preventDefault();
+});
