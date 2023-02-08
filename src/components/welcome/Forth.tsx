fe838bdde8a65eec54b296a9cae72842920363cc
david361215 <1138890119@qq.com>
2023-02-08 13:13:05 +0800

    提取 WelcomeLayout 组件

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,8 +1,22 @@
 import { defineComponent } from 'vue';
+import s from './WelcomeLayout.module.scss';
+import cloud from '../../assets/icons/cloud.svg';
+import { RouterLink } from 'vue-router';
+import { WelcomeLayout } from './WelcomeLayout';
+
 export const Forth = defineComponent({
   setup: (props, context) => {
+    const slots = {
+      icon: () => <img src={cloud} />,
+      title: () => <h2>云备份<br />再也不怕数据丢失</h2>,
+      buttons: () => <>
+        <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
+        <RouterLink to='/start'>开启应用</RouterLink>
+        <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
+      </>
+    }
     return () => (
-      <div>Forth</div>
+      <WelcomeLayout v-slots={slots}></WelcomeLayout>
     )
   }
 })
--- src/components/welcome/Second.tsx
+++ src/components/welcome/Second.tsx
@@ -1,8 +1,22 @@
 import { defineComponent } from 'vue';
+import s from './WelcomeLayout.module.scss';
+import alarm from '../../assets/icons/alarm.svg';
+import { RouterLink } from 'vue-router';
+import { WelcomeLayout } from './WelcomeLayout';
+
 export const Second = defineComponent({
   setup: (props, context) => {
+    const slots = {
+      icon: () => <img src={alarm} />,
+      title: () => <h2>每日提醒<br />不会遗漏每一笔账单</h2>,
+      buttons: () => <>
+        <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
+        <RouterLink to='/welcome/3'>下一页</RouterLink>
+        <RouterLink to='/start'>跳过</RouterLink>
+      </>
+    }
     return () => (
-      <div>Second</div>
+      <WelcomeLayout v-slots={slots}></WelcomeLayout>
     )
   }
 })
