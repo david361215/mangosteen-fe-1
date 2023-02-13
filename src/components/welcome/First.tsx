91fa1016846d24c49baab3a8978d4dbc21dfbb0f
david361215 <1138890119@qq.com>
2023-02-13 12:30:58 +0800

    使用 SVG Sprites

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,10 +1,11 @@
 import s from './welcome.module.scss';
-import piggy from '../../assets/icons/piggy.svg';
 import { FunctionalComponent } from 'vue';
 
 export const First: FunctionalComponent = () => {
   return <div class={s.card}>
-    <img src={piggy} />
+    <svg>
+      <use xlinkHref='#piggy'></use>
+    </svg>
     <h2>会挣钱<br />还要会省钱</h2>
   </div>
 }
--- src/components/welcome/Third.tsx
+++ src/components/welcome/Third.tsx
@@ -1,10 +1,11 @@
 import s from './welcome.module.scss';
-import chart from '../../assets/icons/chart.svg';
 import { FunctionalComponent } from 'vue';
 
 export const Third: FunctionalComponent = () => {
   return <div class={s.card}>
-    <img src={chart} />
+    <svg>
+      <use xlinkHref='#chart'></use>
+    </svg>
     <h2>数据可视化<br />收支一目了然</h2>
   </div>
 }
