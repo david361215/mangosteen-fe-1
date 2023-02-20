f396d56242cc02113f7f10aefa24f66b91051ab4
david361215 <1138890119@qq.com>
2023-02-20 12:41:35 +0800

    创建 Center、Navbar、Overlay 组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,19 +1,35 @@
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { Button } from '../shared/components/Button';
+import { Center } from '../shared/components/Center';
 import { FloatButton } from '../shared/components/FloatButton';
+import { Icon } from '../shared/components/Icon';
+import { Navbar } from '../shared/components/Navbar';
+import { Overlay } from '../shared/components/Overlay';
 import s from './StartPage.module.scss';
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
-    const onClick = () => {
-      console.log('hi');
+    const overlayVisible = ref(false)
+    const onClickMenu = () => {
+      overlayVisible.value = !overlayVisible.value
     }
     return () => (
       <div>
+        <Navbar>{
+          {
+            default: () => '山竹记账',
+            icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
+          }
+        }</Navbar>
+        <Center class={s.piggy_wrapper}>
+          <Icon name="piggy" class={s.piggy} />
+        </Center>
         <div class={s.button_wrapper}>
-          <Button class={s.button} onClick={onClick}>测试</Button>
+          <Button class={s.button} >开始记账</Button>
         </div>
         <FloatButton iconName='add' />
+        {overlayVisible.value &&
+          <Overlay onClose={() => { overlayVisible.value = false }} />}
       </div>
     )
   }
