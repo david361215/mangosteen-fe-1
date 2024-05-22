342be973b936f9261199303c993d63e29727e1c2
david361215 <1138890119@qq.com>
2024-05-22 14:07:59 +0800

    登录页面会自动跳转到之前的页面

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -7,7 +7,7 @@ import { hasError, validate } from '../shared/validate';
 import s from './SignInPage.module.scss';
 import { httpClient } from '../shared/HttpClient';
 import { useBool } from '../hooks/useBool';
-import { history } from '../shared/history';
+import { useRoute, useRouter } from 'vue-router';
 export const SignInPage = defineComponent({
   setup: (props, context) => {
     const formData = reactive({
@@ -20,6 +20,8 @@ export const SignInPage = defineComponent({
     })
     const refValidationCode = ref<any>()
     const { ref: refDisabled, toggle, on: disabled, off: enable } = useBool(false)
+    const router = useRouter()
+    const route = useRoute()
     const onSubmit = async (e: Event) => {
       e.preventDefault()
       Object.assign(errors, {
@@ -33,8 +35,11 @@ export const SignInPage = defineComponent({
       ]))
       if (!hasError(errors)) {
         const response = await httpClient.post<{ jwt: string }>('/session', formData)
+          .catch(onError)
         localStorage.setItem('jwt', response.data.jwt)
-        history.push('/')
+        // router.push('/sign_in?return_to=' + encodeURIComponent(route.fullPath))
+        const returnTo = route.query.return_to?.toString()
+        router.push(returnTo || '/')
       }
     }
     const onError = (error: any) => {
