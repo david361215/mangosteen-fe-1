2fc9b46729680325916609a1e3678aed4bb95a87
david361215 <1138890119@qq.com>
2024-05-23 17:33:08 +0800

    创建 mock 模块

--- src/shared/HttpClient.ts
+++ src/shared/HttpClient.ts
@@ -1,5 +1,10 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
-type JSONValue = string | number | null | boolean | JSONValue[] | { [key: string]: JSONValue };
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import { mockSession } from "../mock/mock";
+
+type GetConfig = Omit<AxiosRequestConfig, 'params' | 'url' | 'method'>
+type PostConfig = Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>
+type PatchConfig = Omit<AxiosRequestConfig, 'url' | 'data'>
+type DeleteConfig = Omit<AxiosRequestConfig, 'params'>
 
 export class HttpClient {
   instance: AxiosInstance
@@ -8,22 +13,37 @@ export class HttpClient {
       baseURL
     })
   }
-  // read
-  get<R = unknown>(url: string, query?: Record<string, string>, config?: Omit<AxiosRequestConfig, 'params' | 'url' | 'method'>) {
+
+  get<R = unknown>(url: string, query?: Record<string, JSONValue>, config?: GetConfig) {
     return this.instance.request<R>({ ...config, url: url, params: query, method: 'get' })
   }
-  // create
-  post<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>) {
+
+  post<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: PostConfig) {
     return this.instance.request<R>({ ...config, url, data, method: 'post' })
   }
-  // update
-  patch<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: Omit<AxiosRequestConfig, 'url' | 'data'>) {
+
+  patch<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: PatchConfig) {
     return this.instance.request<R>({ ...config, url, data, method: 'patch' })
   }
-  // destroy
-  delete<R = unknown>(url: string, query?: Record<string, string>, config?: Omit<AxiosRequestConfig, 'params'>) {
+
+  delete<R = unknown>(url: string, query?: Record<string, string>, config?: DeleteConfig) {
     return this.instance.request<R>({ ...config, url: url, params: query, method: 'delete' })
   }
+}
+
+const mock = (response: AxiosResponse) => {
+  if (location.hostname !== 'localhost'
+    && location.hostname !== '127.0.0.1'
+    && location.hostname !== '192.168.3.57') { return false }
+  switch (response.config?.params?._mock) {
+    case 'tagIndex':
+      [response.status, response.data] = mockTagIndex(response.config)
+      return true
+    case 'session':
+      [response.status, response.data] = mockSession(response.config)
+      return true
+  }
+  return false
 }
 
 export const httpClient = new HttpClient('/api/v1')
@@ -36,14 +56,26 @@ httpClient.instance.interceptors.request.use(config => {
   return config
 })
 
-httpClient.instance.interceptors.response.use(response => {
+httpClient.instance.interceptors.response.use((response) => {
+  mock(response)
   return response
 }, (error) => {
-  if (error.response) {
-    const axiosError = error as AxiosError
-    if (axiosError.response?.status === 429) {
-      alert('你太频繁了')
-    }
+  if (mock(error.response)) {
+    return error.response
+  } else {
+    throw error
   }
-  throw error
 })
+
+httpClient.instance.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response) {
+      const axiosError = error as AxiosError
+      if (axiosError.response?.status === 429) {
+        alert('你太频繁了')
+      }
+    }
+    throw error
+  }
+)
--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -35,8 +35,10 @@ export const SignInPage = defineComponent({
         { key: 'code', type: 'required', message: '必填' }
       ]))
       if (!hasError(errors)) {
-        const response = await httpClient.post<{ jwt: string }>('/session', formData)
-          .catch(onError)
+        const response = await httpClient.post<{ jwt: string }>('/session', formData, {
+          params: { _mock: 'session' }
+        }).catch(onError)
+        console.log(response)
         localStorage.setItem('jwt', response.data.jwt)
         // router.push('/sign_in?return_to=' + encodeURIComponent(route.fullPath))
         const returnTo = route.query.return_to?.toString()
