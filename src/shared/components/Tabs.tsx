f93700a67909a7df7c33152564f6e3bb47d3886b
david361215 <1138890119@qq.com>
2024-05-28 14:41:14 +0800

    封装 Tags 组件

--- src/shared/components/Tabs.tsx
+++ src/shared/components/Tabs.tsx
@@ -33,7 +33,16 @@ export const Tabs = defineComponent({
             </li>)}
         </ol>
         <div>
-          {tabs.find(item => item.props?.name === props?.selected)}
+          {tabs.map(item =>
+            <div v-show={item.props?.name === props?.selected}>
+              {item}
+            </div>
+            // <div style={{
+            //   display: item.props?.name === props?.selected ? 'block' : 'none'
+            // }}>
+            //   {item}
+            // </div>
+          )}
         </div>
       </div>
     }
