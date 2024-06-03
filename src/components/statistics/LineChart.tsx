6027a92d2f610f1bc55595be662897db15c8aba8
david361215 <1138890119@qq.com>
2024-06-03 15:20:26 +0800

    修改 prettier 配置

--- src/components/statistics/LineChart.tsx
+++ src/components/statistics/LineChart.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, onMounted, ref, watch } from 'vue'
+import { PropType, defineComponent, onMounted, ref, watch } from 'vue'
 import s from './LineChart.module.scss'
 import * as echarts from 'echarts'
 import { Time } from '../../shared/Time'
@@ -11,18 +11,18 @@ const echartsOption = {
     formatter: ([item]: any) => {
       const [x, y] = item.data
       return `${new Time(new Date(x)).format('YYYY年MM月DD日')} ￥${getMoney(y)}`
-    },
+    }
   },
   grid: [{ left: 16, top: 20, right: 16, bottom: 20 }],
   xAxis: {
     type: 'time',
     boundaryGap: ['3%', '0%'],
     axisLabel: {
-      formatter: (value: string) => new Time(new Date(value)).format('MM-DD'),
+      formatter: (value: string) => new Time(new Date(value)).format('MM-DD')
     },
     axisTick: {
-      alignWithLabel: true,
-    },
+      alignWithLabel: true
+    }
   },
   yAxis: {
     show: true,
@@ -30,21 +30,21 @@ const echartsOption = {
     splitLine: {
       show: true,
       lineStyle: {
-        type: 'dashed',
-      },
+        type: 'dashed'
+      }
     },
     axisLabel: {
-      show: false,
-    },
-  },
+      show: false
+    }
+  }
 }
 
 export const LineChart = defineComponent({
   props: {
     data: {
       type: Array as PropType<[string, number][]>,
-      required: true,
-    },
+      required: true
+    }
   },
   setup: (props, context) => {
     const refDiv = ref<HTMLDivElement>()
@@ -61,9 +61,9 @@ export const LineChart = defineComponent({
         series: [
           {
             data: props.data,
-            type: 'line',
-          },
-        ],
+            type: 'line'
+          }
+        ]
       })
     })
     watch(
@@ -72,13 +72,13 @@ export const LineChart = defineComponent({
         chart?.setOption({
           series: [
             {
-              data: props.data,
-            },
-          ],
+              data: props.data
+            }
+          ]
         })
-      },
+      }
     )
 
     return () => <div ref={refDiv} class={s.wrapper}></div>
-  },
+  }
 })
