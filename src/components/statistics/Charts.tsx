b024dab71b242ca6f2f7fce7121dda402f9db28d
david361215 <1138890119@qq.com>
2024-06-05 09:44:06 +0800

    完成 bar chart

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -60,7 +60,13 @@ export const Charts = defineComponent({
         value: item.amount
       }))
     )
-
+    const betterData3 = computed<{ tag: Tag; amount: number; percent: number }[]>(() => {
+      const total = data2.value.reduce((sum, item) => sum + item.amount, 0)
+      return data2.value.map((item) => ({
+        ...item,
+        percent: Math.round((item.amount / total) * 100)
+      }))
+    })
     onMounted(async () => {
       const response = await httpClient.get<{ groups: Data2; summary: number }>('/items/summary', {
         happen_after: props.startDate,
@@ -84,7 +90,7 @@ export const Charts = defineComponent({
         />
         <LineChart data={betterData1.value} />
         <PieChart data={betterData2.value} />
-        <Bars />
+        <Bars data={betterData3.value} />
       </div>
     )
   }
