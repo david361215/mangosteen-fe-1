723a185b9783899e87d901bdbea1f4ca9eed8c1e
david361215 <1138890119@qq.com>
2024-06-05 16:05:06 +0800

    修复日期选择器默认时间的bug

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,8 +1,8 @@
-import { defineComponent, PropType, ref } from 'vue';
-import { Icon } from '../../shared/components/Icon';
-import { DatetimePicker, Popup } from 'vant';
-import { Time } from '../../shared/Time';
-import s from './InputPad.module.scss';
+import { defineComponent, PropType, ref } from 'vue'
+import { Icon } from '../../shared/components/Icon'
+import { DatetimePicker, Popup } from 'vant'
+import { Time } from '../../shared/Time'
+import s from './InputPad.module.scss'
 
 export const InputPad = defineComponent({
   props: {
@@ -10,16 +10,16 @@ export const InputPad = defineComponent({
     amount: Number,
     onSubmit: {
       type: Function as PropType<() => void>
-    }  
+    }
   },
-  emits:['update:happenAt','update:amount'],
+  emits: ['update:happenAt', 'update:amount'],
   setup: (props, context) => {
-    const refAmount = ref( props.amount? (props.amount/100).toString() : '0')
+    const refAmount = ref(props.amount ? (props.amount / 100).toString() : '0')
     const refDatePickerVisible = ref(false)
-    const showDatePicker = () => refDatePickerVisible.value = true
-    const hideDatePicker = () => refDatePickerVisible.value = false
+    const showDatePicker = () => (refDatePickerVisible.value = true)
+    const hideDatePicker = () => (refDatePickerVisible.value = false)
     const setDate = (date: Date) => {
-      context.emit('update:happenAt',date.toISOString())
+      context.emit('update:happenAt', date.toISOString())
       hideDatePicker()
     }
     const appendText = (n: number | string) => {
@@ -31,7 +31,7 @@ export const InputPad = defineComponent({
       if (dotIndex >= 0 && refAmount.value.length - dotIndex > 2) {
         return
       }
-      if (dotIndex >= 0 && nString === ".") {
+      if (dotIndex >= 0 && nString === '.') {
         return
       }
       if (refAmount.value === '0' && '0123456789'.indexOf(nString) >= 0) {
@@ -47,47 +47,120 @@ export const InputPad = defineComponent({
         return
       }
       refAmount.value = refAmount.value.substring(0, length - 1)
-
     }
     const buttons = [
-      { text: '1', onclick: () => { appendText(1) } },
-      { text: '2', onclick: () => { appendText(2) } },
-      { text: '3', onclick: () => { appendText(3) } },
-      { text: '4', onclick: () => { appendText(4) } },
-      { text: '5', onclick: () => { appendText(5) } },
-      { text: '6', onclick: () => { appendText(6) } },
-      { text: '7', onclick: () => { appendText(7) } },
-      { text: '8', onclick: () => { appendText(8) } },
-      { text: '9', onclick: () => { appendText(9) } },
-      { text: '.', onclick: () => { appendText('.') } },
-      { text: '0', onclick: () => { appendText(0) } },
-      { text: '清空', onclick: () => { refAmount.value = '0' } },
-      { text: '提交',         
+      {
+        text: '1',
+        onclick: () => {
+          appendText(1)
+        }
+      },
+      {
+        text: '2',
+        onclick: () => {
+          appendText(2)
+        }
+      },
+      {
+        text: '3',
+        onclick: () => {
+          appendText(3)
+        }
+      },
+      {
+        text: '4',
+        onclick: () => {
+          appendText(4)
+        }
+      },
+      {
+        text: '5',
+        onclick: () => {
+          appendText(5)
+        }
+      },
+      {
+        text: '6',
+        onclick: () => {
+          appendText(6)
+        }
+      },
+      {
+        text: '7',
+        onclick: () => {
+          appendText(7)
+        }
+      },
+      {
+        text: '8',
+        onclick: () => {
+          appendText(8)
+        }
+      },
+      {
+        text: '9',
+        onclick: () => {
+          appendText(9)
+        }
+      },
+      {
+        text: '.',
+        onclick: () => {
+          appendText('.')
+        }
+      },
+      {
+        text: '0',
+        onclick: () => {
+          appendText(0)
+        }
+      },
+      {
+        text: '清空',
+        onclick: () => {
+          refAmount.value = '0'
+        }
+      },
+      {
+        text: '提交',
         onclick: () => {
           context.emit('update:amount', parseFloat(refAmount.value) * 100)
           props.onSubmit?.()
         }
       },
-      { text: '删除', onclick: () => { removeText() } }
+      {
+        text: '删除',
+        onclick: () => {
+          removeText()
+        }
+      }
     ]
-    return () => (<>
-      <div class={s.dateAndAmount}>
-        <span class={s.date}>
-          <Icon name="date" class={s.icon} />
-          <span>
-            <span onClick={showDatePicker}>{new Time(props.happenAt).format()}</span>
-            <Popup position='bottom' v-model:show={refDatePickerVisible.value}>
-              <DatetimePicker value={props.happenAt} type="date" title="选择年月日" onConfirm={setDate} onCancel={hideDatePicker} />
-            </Popup>
+    return () => (
+      <>
+        <div class={s.dateAndAmount}>
+          <span class={s.date}>
+            <Icon name="date" class={s.icon} />
+            <span>
+              <span onClick={showDatePicker}>{new Time(props.happenAt).format()}</span>
+              <Popup position="bottom" v-model:show={refDatePickerVisible.value}>
+                <DatetimePicker
+                  modelValue={props.happenAt? new Date(props.happenAt): new Date()}
+                  type="date"
+                  title="选择年月日"
+                  onConfirm={setDate}
+                  onCancel={hideDatePicker}
+                />
+              </Popup>
+            </span>
           </span>
-        </span>
-        <span class={s.amount}>{refAmount.value}</span>
-      </div>
-      <div class={s.buttons}>
-        {buttons.map(button =>
-          <button onClick={button.onclick}>{button.text}</button>)}
-      </div>
-    </>
+          <span class={s.amount}>{refAmount.value}</span>
+        </div>
+        <div class={s.buttons}>
+          {buttons.map((button) => (
+            <button onClick={button.onclick}>{button.text}</button>
+          ))}
+        </div>
+      </>
     )
   }
 })
--- src/shared/components/Form.tsx
+++ src/shared/components/Form.tsx
@@ -1,9 +1,9 @@
-import { DatetimePicker, Popup } from 'vant';
-import { defineComponent, PropType, computed, ref } from 'vue';
-import { Time } from '../Time';
-import { Button } from './Button';
-import { EmojiSelect } from './EmojiSelect';
-import s from './Form.module.scss';
+import { DatetimePicker, Popup } from 'vant'
+import { defineComponent, PropType, computed, ref } from 'vue'
+import { Time } from '../Time'
+import { Button } from './Button'
+import { EmojiSelect } from './EmojiSelect'
+import s from './Form.module.scss'
 export const Form = defineComponent({
   props: {
     onSubmit: {
@@ -25,7 +25,7 @@ export const FormItem = defineComponent({
       type: String
     },
     modelValue: {
-      type: [String, Number]
+      type: [String, Number, Date]
     },
     type: {
       type: String as PropType<'text' | 'emojiSelect' | 'date' | 'validationCode' | 'select'>
@@ -37,78 +37,104 @@ export const FormItem = defineComponent({
       type: String
     },
     options: {
-      type: Array as PropType<Array<{ value: string, text: string }>>
+      type: Array as PropType<Array<{ value: string; text: string }>>
     },
     onClick: Function as PropType<() => void>,
     countFrom: {
       type: Number,
       default: 60
     },
-    disabled: Boolean,
+    disabled: Boolean
   },
   emits: ['update:modelValue'],
   setup: (props, context) => {
     const refDateVisible = ref(false)
     const timer = ref<number>()
     const count = ref<number>(props.countFrom)
-    const isCounting = computed(()=> !!timer.value)
+    const isCounting = computed(() => !!timer.value)
     const startCount = () =>
-      timer.value = setInterval(()=>{
+      (timer.value = setInterval(() => {
         count.value -= 1
-        if (count.value === 0){
+        if (count.value === 0) {
           clearInterval(timer.value)
           timer.value = undefined
           count.value = props.countFrom
         }
-      }, 1000)
+      }, 1000))
     context.expose({ startCount })
     const content = computed(() => {
       switch (props.type) {
         case 'text':
-          return <input
-            value={props.modelValue}
-            placeholder={props.placeholder}
-            onInput={(e: any) => context.emit('update:modelValue', e.target.value)}
-            class={[s.formItem, s.input]} />
-        case 'emojiSelect':
-          return <EmojiSelect
-            modelValue={props.modelValue?.toString()}
-            onUpdate:modelValue={value => context.emit('update:modelValue', value)}
-            class={[s.formItem, s.emojiList, s.error]} />
-        case 'date':
-          return <>
-            <input readonly={true} value={props.modelValue}
-              placeholder={props.placeholder}
-              onClick={() => { refDateVisible.value = true }}
-              class={[s.formItem, s.input]} />
-            <Popup position='bottom' v-model:show={refDateVisible.value}>
-              <DatetimePicker value={props.modelValue} type="date" title="选择年月日"
-                onConfirm={(date: Date) => {
-                  context.emit('update:modelValue', new Time(date).format())
-                  refDateVisible.value = false
-                }}
-                onCancel={() => refDateVisible.value = false} ></DatetimePicker>
-            </Popup >
-          </>
-        case 'validationCode':
-          return <>
+          return (
             <input
               value={props.modelValue}
+              placeholder={props.placeholder}
               onInput={(e: any) => context.emit('update:modelValue', e.target.value)}
-              class={[s.formItem, s.input, s.validationCodeInput]}
-              placeholder={props.placeholder} />
-            <Button disabled={isCounting.value || props.disabled} onClick={props.onClick} class={[s.formItem, s.button, s.validationCodeButton]}>
-              {isCounting.value? `${count.value}秒后可重新发送` : '发送验证码'}
-            </Button>
-          </>
+              class={[s.formItem, s.input]}
+            />
+          )
+        case 'emojiSelect':
+          return (
+            <EmojiSelect
+              modelValue={props.modelValue?.toString()}
+              onUpdate:modelValue={(value) => context.emit('update:modelValue', value)}
+              class={[s.formItem, s.emojiList, s.error]}
+            />
+          )
+        case 'date':
+          return (
+            <>
+              <input
+                readonly={true}
+                value={props.modelValue}
+                placeholder={props.placeholder}
+                onClick={() => {
+                  refDateVisible.value = true
+                }}
+                class={[s.formItem, s.input]}
+              />
+              <Popup position="bottom" v-model:show={refDateVisible.value}>
+                <DatetimePicker
+                  modelValue={props.modelValue ? new Date(props.modelValue) : new Date()}
+                  type="date"
+                  title="选择年月日"
+                  onConfirm={(date: Date) => {
+                    context.emit('update:modelValue', new Time(date).format())
+                    refDateVisible.value = false
+                  }}
+                  onCancel={() => (refDateVisible.value = false)}
+                ></DatetimePicker>
+              </Popup>
+            </>
+          )
+        case 'validationCode':
+          return (
+            <>
+              <input
+                value={props.modelValue}
+                onInput={(e: any) => context.emit('update:modelValue', e.target.value)}
+                class={[s.formItem, s.input, s.validationCodeInput]}
+                placeholder={props.placeholder}
+              />
+              <Button
+                disabled={isCounting.value || props.disabled}
+                onClick={props.onClick}
+                class={[s.formItem, s.button, s.validationCodeButton]}
+              >
+                {isCounting.value ? `${count.value}秒后可重新发送` : '发送验证码'}
+              </Button>
+            </>
+          )
         case 'select':
-          return <select value={props.modelValue}
-            onChange={(e: any) => context.emit('update:modelValue', e.target.value)}
-            class={[s.formItem, s.select]}>
-            {props.options?.map(option =>
-              <option value={option.value}>{option.text}</option>
-            )}
-          </select>
+          return (
+            <select
+              value={props.modelValue}
+              onChange={(e: any) => context.emit('update:modelValue', e.target.value)}
+              class={[s.formItem, s.select]}
+            >
+              {props.options?.map((option) => <option value={option.value}>{option.text}</option>)}
+            </select>
+          )
         case undefined:
           return context.slots.default?.()
       }
@@ -116,14 +142,8 @@ export const FormItem = defineComponent({
     return () => (
       <div class={s.formRow}>
         <label class={s.formLabel}>
-          {props.label &&
-            <span class={s.formItem_name}>
-              {props.label}
-            </span>
-          }
-          <div class={s.formItem_value}>
-            {content.value}
-          </div>
+          {props.label && <span class={s.formItem_name}>{props.label}</span>}
+          <div class={s.formItem_value}>{content.value}</div>
           <div class={s.formItem_errorHint}>
             <span>{props.error ?? '　'}</span>
           </div>
